1356029d49b93840535ce41d3e9157f28489ad5e
May C <maycheongs@gmail.com>
2021-03-02 01:10:15 +0000

    exercise 4 - conditional where

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,31 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
-puts "Exercise 4"
-puts "----------"
+puts 'Exercise 4'
+puts '----------'
 
 # Your code goes here ...
+@store3 = Store.create(
+  name: 'Surrey',
+  annual_revenue: 224_000,
+  mens_apparel: false,
+  womens_apparel: true,
+)
+@store4 = Store.create(
+  name: 'Whistler',
+  annual_revenue: 1_900_000,
+  mens_apparel: true,
+  womens_apparel: false,
+)
+@store5 = Store.create(
+  name: 'Yaletown',
+  annual_revenue: 430_000,
+  mens_apparel: false,
+  womens_apparel: true,
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "Store: #{store.name} Annual: #{store.annual_revenue}"
+end
